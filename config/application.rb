e79811d1b08fc9409114b20af6d36854dab4e0e6
NTD <kaanoo2904@gmail.com>
2016-08-21 18:18:39 +0700

    try to turn on config.assets.compile = true

--- config/application.rb
+++ config/application.rb
@@ -7,9 +7,10 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module MarugotoBackend
-  class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-  end
+    class Application < Rails::Application
+        # Settings in config/environments/* take precedence over those specified here.
+        # Application configuration should go into files in config/initializers
+        # -- all .rb files in that directory are automatically loaded.
+        config.serve_static_assets = true
+    end
 end
