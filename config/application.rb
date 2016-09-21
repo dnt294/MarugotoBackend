b6668504d40e0a367ebed8cbb6f340502ebe34f9
NTD <kaanoo2904@gmail.com>
2016-09-21 18:28:30 +0700

    add ckeditor to autoloadpaths

--- config/application.rb
+++ config/application.rb
@@ -8,11 +8,13 @@ module MarugotoBackend
     class Application < Rails::Application
 
         #config.autoload_paths += Dir[Rails.root.join('app','models','{*/}')]
+
         config.assets.precompile += Ckeditor.assets
 
-        config.assets.precompile += %w( ckeditor/my_config.js ckeditor/my_styles.js ckeditor/	my_contents.css ckeditor/filebrowser/*)
+        config.assets.precompile += %w( ckeditor/my_config.js ckeditor/	my_contents.css ckeditor/filebrowser/*)
+        config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
 
-        config.serve_static_assets = true
-    end
-end
+                                    config.serve_static_assets = true
+                                    end
+                                    end
