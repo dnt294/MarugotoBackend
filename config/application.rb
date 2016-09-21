2d52130cb2e412501a7c52869fcd63d57272eaf3
NTD <kaanoo2904@gmail.com>
2016-09-21 18:18:39 +0700

    try to add ckeditor assets to precompile

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,11 @@ module MarugotoBackend
     class Application < Rails::Application
 
         #config.autoload_paths += Dir[Rails.root.join('app','models','{*/}')]
+        config.assets.precompile += Ckeditor.assets
+
         config.assets.precompile += %w( ckeditor/my_config.js ckeditor/my_styles.js ckeditor/	my_contents.css ckeditor/filebrowser/*)
+
+
         config.serve_static_assets = true
     end
 end
